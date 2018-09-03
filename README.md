1e1748f4a672d090e5fc21ca616fda4c5f98df12
Eduardo Pilla Portilho <eduardo@portilho.com>
2018-09-03 08:32:29 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Movie Trailer Website
+
+This project consists of server-side code to store a list of my favorite movies and generate a static web page allowing visitors to browse the movies and watch trailers.
+
+# How to run?
+
+This project requires Python 2.
+
+* Clone or download the project from https://github.com/eduardoportilho/create-a-movie-website
+* ´cd´ into its directory
+* Run `python movie_website_generator.py`
+* The file `fresh_tomatoes.html` will be generated in the current directory and opened in the default browser.
---
+++ movie_website_generator.py
@@ -0,0 +1,45 @@
+import movie
+import fresh_tomatoes
+
+# Create instances of the `Movie` class for each movie
+capitain_fantastic = movie.Movie(
+  'Captain Fantastic',
+  2016,
+  'https://upload.wikimedia.org/wikipedia/en/c/c2/Captain_Fantastic_poster.jpg',
+  'https://www.youtube.com/watch?v=D1kH4OMIOMc')
+
+gone_girl = movie.Movie(
+  'Gone Girl',
+  2014,
+  'https://upload.wikimedia.org/wikipedia/en/0/05/Gone_Girl_Poster.jpg',
+  'https://www.youtube.com/watch?v=2-_-1nJf8Vg')
+
+prisoners = movie.Movie(
+  'Prisoners',
+  2013,
+  'https://upload.wikimedia.org/wikipedia/en/6/63/Prisoners2013Poster.jpg',
+  'https://www.youtube.com/watch?v=bpXfcTF6iVk')
+
+lotr_fellowship = movie.Movie(
+  'The Lord of the Rings: The Fellowship of the Ring',
+  2001,
+  'https://upload.wikimedia.org/wikipedia/en/9/9d/The_Lord_of_the_Rings_The_Fellowship_of_the_Ring_%282001%29_theatrical_poster.jpg',
+  'https://www.youtube.com/watch?v=Pki6jbSbXIY')
+
+lotr_two_towers = movie.Movie(
+  'The Lord of the Rings: The Two Towers',
+  2002,
+  'https://upload.wikimedia.org/wikipedia/en/a/ad/Lord_of_the_Rings_-_The_Two_Towers.jpg',
+  'https://www.youtube.com/watch?v=2dlRvAjU_RI')
+
+lotr_return_king = movie.Movie(
+  'The Lord of the Rings: The Return of the King',
+  2003,
+  'https://upload.wikimedia.org/wikipedia/en/9/9d/Lord_of_the_Rings_-_The_Return_of_the_King.jpg',
+  'https://www.youtube.com/watch?v=r5X-hFf6Bwo')
+
+# Create a list with all the movies
+movies = [capitain_fantastic, gone_girl, prisoners, lotr_fellowship, lotr_two_towers, lotr_return_king]
+
+# Generate the page and open it on the default browser
+fresh_tomatoes.open_movies_page(movies)
